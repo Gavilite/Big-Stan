d46f1d6adef90dee91d22b04e44fbcb59fb3fc26
Gavilite <gavilite@gmail.com>
2022-11-19 09:22:43 -0800

    personal work

--- bhells/hello.c
+++ bhells/hello.c
@@ -5,11 +5,13 @@ int main()
 	int age;
 	age = 30;
 	int year = 1992;
+	int time = 17202211;
 
 	printf("Hello World!, My name on the next line\n");
 	printf(" My name is Gavi, \n");
 	printf("My name is Gavi ... %i \n", age);
 	printf(" I am %i and i was born on %i \n", age ,year);
+	printf("Chikamso and Funny wedding was on %d \n", time);
 
 	return 0;
 }
